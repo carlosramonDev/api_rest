8a2c4df4616aeeb298bffd2a7eccdc6cf8a603a7
carlosramonDev <carlosramonDev@gmail.com>
2023-11-06 23:24:29 -0300

    :bulb: raw: change student email

---
+++ src/database/migrations/20231107014318-change-student-email-unique.js
@@ -0,0 +1,13 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.changeColumn(
+    'students',
+    'email',
+    {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  ),
+
+  down: () => {},
+};
